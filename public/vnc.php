c33f5a09ed0c0c64eea050457a57a8dcc4e9368d
Charlie Root <root@gh.convectix.com>
2022-07-31 16:14:35 +0000

    vnc: HTTP_HOST is preferred

--- public/vnc.php
+++ public/vnc.php
@@ -16,10 +16,32 @@ function runVNC($jname)
 
 	CBSD::run("vm_vncwss jname=%s permit=%s", array($jname,$remote_ip));
 
-	if(isset($_SERVER['SERVER_NAME']) && !empty(trim($_SERVER['SERVER_NAME']))){
-		$nodeip=$_SERVER['SERVER_NAME'];
+	// HTTP_HOST is preferred for href
+	if (isset($_SERVER['HTTP_HOST']) && !empty(trim($_SERVER['HTTP_HOST']))){
+		$nodeip=$_SERVER['HTTP_HOST'];
+	}
+
+	if (filter_var($nodeip, FILTER_VALIDATE_IP)) {
+		$is_ip4=true;
 	} else {
-		$nodeip=$_SERVER['SERVER_ADDR'];
+		$is_ip4=false;
+	}
+
+	if ($is_ip4 == false) {
+		if (filter_var($nodeip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+			$is_ip6=true;
+		} else {
+			$is_ip6=false;
+		}
+	}
+
+	// HTTP_HOST is IP, try to check SERVER_NAME
+	if (($is_ip4==true)||($is_ip6==true)) {
+		if(isset($_SERVER['SERVER_NAME']) && !empty(trim($_SERVER['SERVER_NAME']))){
+			$nodeip=$_SERVER['SERVER_NAME'];
+		} else {
+			$nodeip=$_SERVER['SERVER_ADDR'];
+		}
 	}
 
 	// handle when 'server_name _;' - use IP instead

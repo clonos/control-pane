560ccd53b90b971a5cfa7a45046809e7e31ef6a9
olevole <olevole@olevole.ru>
2025-07-24 15:09:03 +0300

    it seems that these files should not be in git at all. Moreover, we will have to move the cache directory to a separate directory (e.g. /cache/) with the appropriate (write) permissions. And besides, the cache should be per-session (or per-user) - WIP. #74, thanks @dablyputs

--- public/dialogs/_translate.cache/ru.tasklog.php
+++
@@ -1,9 +0,0 @@
-<dialog id="tasklog" class="window-box tasklog">
-	<h1><span id="trlt-347">Task log</span></h1>
-	<div class="window-content">
-		<span id="trlt-348">Task log here&hellip;</span>
-	</div>
-	<div class="buttons">
-		<input type="button" value="Закрыть" class="button red cancel-but" />
-	</div>
-</dialog>
